b39c7bf41c64ce62bb331669198c58f3d445f7d3
MarcosAndreBueno <www.bueno@yahoo.com>
2023-01-14 22:06:07 -0300

    add: insert data

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
 import model.dao.DaoFactory;
-import model.dao.SellerDao;
-import model.entities.Seller;
-import model.entities.Department;
+import java.util.Date;
 import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -46,6 +48,12 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+//		System.out.println("\n=== TEST 4: seller insert =====");
+//		Department department = new Department(2, null);
+//		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+//		sellerDao.insert(newSeller);
+//		System.out.println("Inserted! New id = " + newSeller.getId());
 
 	} 
 }
